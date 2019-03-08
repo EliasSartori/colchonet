a23697cbe71d5f136c5c65d054b367cdc3dd92c5
eliassartori <elias.sartori@gmail.com>
2019-03-08 10:52:48 -0300

    Change README, improving templates, add validations room model

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,10 @@
 class Room < ApplicationRecord
+
+    validates :title, presence: true
+    validates :location, presence: true
+    validates :description, length: { minimum: 30 }, allow_blank: false
+
+    def complete_name
+        "#{title}, #{location}"
+    end
 end
